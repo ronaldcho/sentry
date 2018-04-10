4188d7c1ce5abfd2dff9e43bc1c3286becf2e7b0
Brett Hoerner <brett@bretthoerner.com>
2018-04-10 14:18:46 -0500

    Implementation v1.

--- src/sentry/tagstore/snuba/backend.py
+++ src/sentry/tagstore/snuba/backend.py
@@ -8,13 +8,8 @@ sentry.tagstore.snuba.backend
 
 from __future__ import absolute_import
 
-from datetime import datetime, timedelta
-import pytz
-import six
-
 from sentry.tagstore import TagKeyStatus
 from sentry.tagstore.base import TagStorage
-from sentry.utils import snuba
 
 
 class SnubaTagStorage(TagStorage):
@@ -74,35 +69,3 @@ class SnubaTagStorage(TagStorage):
 
     def get_groups_user_counts(self, project_id, group_ids, environment_id):
         pass
-
-    # Search
-    def get_group_ids_for_search_filter(self, project_id, environment_id, tags):
-        from sentry.search.base import ANY, EMPTY
-
-        # Any EMPTY value means there can be no results for this query so
-        # return an empty list immediately.
-        if any(val == EMPTY for _, val in six.iteritems(tags)):
-            return []
-
-        filters = {
-            'environment': [environment_id],
-            'project_id': [project_id],
-        }
-
-        conditions = []
-        for tag, val in six.iteritems(tags):
-            col = 'tags[{}]'.format(tag)
-            if val == ANY:
-                conditions.append((col, 'IS NOT NULL', None))
-            else:
-                conditions.append((col, '=', val))
-
-        end = datetime.utcnow().replace(tzinfo=pytz.UTC)
-        start = end - timedelta(days=90)
-        issues = snuba.query(start, end, ['issue'], conditions, filters)
-
-        # convert
-        #    {issue1: count, ...}
-        # into
-        #    [issue1, ...]
-        return issues.keys()
